38cc64a914e14699e7616f5716b932da01a7a356
name명 <>
2020-10-22 14:01:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kpuescape/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.kpuescape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button bt_1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt_1 = findViewById(R.id.bt_1);
+        bt_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserLogin.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
